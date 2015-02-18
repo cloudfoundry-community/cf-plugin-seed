19b5cb4911d1cbdcac474e5726acc3d1dc51720c
Long Nguyen <long.nguyen11288@gmail.com>
2015-02-18 11:56:08 -0500

    added more tests

--- yaml.go
+++ yaml.go
@@ -16,16 +16,25 @@ type Space struct {
 }
 
 type App struct {
-	Name      string
-	Repo      string `yaml:",omitempty"`
-	Path      string `yaml:",omitempty"`
-	Disk      string `yaml:",omitempty"`
-	Memory    string `yaml:",omitempty"`
-	Instances string `yaml:",omitempty"`
-	Hostname  string `yaml:",omitempty"`
-	Domain    string `yaml:",omitempty"`
-	Buildpack string `yaml:",omitempty"`
-	Manifest  string `yaml:",omitempty"`
+	Name          string
+	Repo          string        `yaml:",omitempty"`
+	Path          string        `yaml:",omitempty"`
+	Disk          string        `yaml:",omitempty"`
+	Memory        string        `yaml:",omitempty"`
+	Instances     string        `yaml:",omitempty"`
+	Hostname      string        `yaml:",omitempty"`
+	Domain        string        `yaml:",omitempty"`
+	Buildpack     string        `yaml:",omitempty"`
+	Manifest      string        `yaml:",omitempty"`
+	ServiceBroker ServiceBroker `yaml:"service_broker,omitempty"`
+	ServiceAccess []Service     `yaml:"service_access,omitempty"`
+}
+
+type ServiceBroker struct {
+	Name     string `yaml:",omitempty"`
+	Username string `yaml:",omitempty"`
+	Password string `yaml:",omitempty"`
+	Url      string `yaml:",omitempty"`
 }
 
 type Service struct {
